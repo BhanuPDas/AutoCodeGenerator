73f735f06d7906393cb1d0546c55bef188ed5cf9
Bhanu Pratap Das <bhanupd199@gmail.com>
2025-11-24 23:23:22 +0100

    Adding code for inference service

--- inference_pipeline/inference_13b.py
+++ inference_pipeline/inference_13b.py
@@ -1,8 +1,6 @@
-from functools import wraps
-
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from prompt_request_model import PromptRequest
 from prompt_response_model import PromptResponse
 import time
@@ -20,58 +18,20 @@ llm = AutoModelForCausalLM.from_pretrained(
 )
 
 
-def measure_request_gpu(func):
-    """
-    Decorator to measure GPU utilization per request
-    and return it alongside inference result.
-    """
-
-    @wraps(func)
-    def wrapper(request: PromptRequest):
-        usage_log = []
-        state = {"monitoring": True}
-
-        def monitor(interval=0.01):
-            while state["monitoring"]:
-                usage_log.append(gpu_stats.get_gpu_stats())
-                time.sleep(interval)
-
-        # Start GPU monitoring
-        t = threading.Thread(target=monitor, daemon=True)
-        t.start()
-
-        # Call the actual inference function
-        result, execution_time, tpms, num_tokens = func(request)
-
-        # Stop monitoring
-        state["monitoring"] = False
-        t.join()
-        if not usage_log:
-            usage_log.append(gpu_stats.get_gpu_stats())
-
-        # Compute GPU stats for this request
-        gpu_util_avg = sum([s['gpu_util_percent'] for s in usage_log]) / len(usage_log)
-        mem_util_avg = sum([s['memory_util_percent'] for s in usage_log]) / len(usage_log)
-        mem_used_avg = sum([s['memory_used_gb'] for s in usage_log]) / len(usage_log)
-
-        # Return result + GPU stats
-        return PromptResponse(
-            result=result,
-            inference_time=execution_time,
-            token_throughput=tpms,
-            num_tokens=num_tokens,
-            gpu_util=round(gpu_util_avg, 1),
-            mem_util=round(mem_util_avg, 1),
-            mem_used=round(mem_used_avg, 1),
-            total_mem=round(usage_log[0]["memory_total_gb"], 1)
-        )
-
-    return wrapper
-
-
 @app.post("/generate")
-@measure_request_gpu
 def generate_code(request: PromptRequest):
+    usage_log = []
+    state = {"monitoring": True}
+
+    def monitor(interval=0.01):
+        while state["monitoring"]:
+            usage_log.append(gpu_stats.get_gpu_stats())
+            time.sleep(interval)
+
+    # Start GPU monitoring
+    t = threading.Thread(target=monitor, daemon=True)
+    t.start()
+
     try:
         start = time.perf_counter()
         formatted_prompt = f"[INST] {request.prompt.strip()} [/INST]"
@@ -83,23 +43,38 @@ def generate_code(request: PromptRequest):
 
         outputs = llm.generate(
             **inputs,
-            do_sample=False,  # Change to True for Sampling runs
+            do_sample=False,
             num_return_sequences=1,
             eos_token_id=tokenizer.eos_token_id,
             max_new_tokens=max_new_tokens,
-            # temperature=0.1,
-            # top_p=0.9,
-            # top_k=50,
         )
+
         gen_ids = outputs[0][inputs["input_ids"].shape[-1]:]
         result = tokenizer.decode(outputs[0], skip_special_tokens=True)
         end = time.perf_counter()
-        execution_time = (end - start)
-        num_tokens = len(gen_ids)
 
-        # token per ms
+        execution_time = end - start
+        num_tokens = len(gen_ids)
         tpms = num_tokens / execution_time
 
-        return result, round(execution_time, 1), round(tpms, 1), round(num_tokens, 1)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        state["monitoring"] = False
+        t.join()
+
+    if not usage_log:
+        usage_log.append(gpu_stats.get_gpu_stats())
+
+    gpu_util_avg = sum([s['gpu_util_percent'] for s in usage_log]) / len(usage_log)
+    mem_util_avg = sum([s['memory_util_percent'] for s in usage_log]) / len(usage_log)
+    mem_used_avg = sum([s['memory_used_gb'] for s in usage_log]) / len(usage_log)
+
+    return PromptResponse(
+        result=result,
+        inference_time=round(execution_time, 1),
+        token_throughput=round(tpms, 1),
+        num_tokens=round(num_tokens, 1),
+        gpu_util=round(gpu_util_avg, 1),
+        mem_util=round(mem_util_avg, 1),
+        mem_used=round(mem_used_avg, 1),
+        total_mem=round(usage_log[0]["memory_total_gb"], 1)
+    )
