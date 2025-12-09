3845152246b625d7ce2979049e898f1ca93f0cfd
Bhanu Pratap Das <bhanupd199@gmail.com>
2025-12-09 12:14:20 +0100

    Adding code for inference service

--- inference_pipeline/inference_13b.py
+++ inference_pipeline/inference_13b.py
@@ -8,7 +8,8 @@ import gpu_stats
 import threading
 
 app = FastAPI()
-model = "meta-llama/CodeLlama-13b-Instruct-hf"
+#model = "meta-llama/CodeLlama-13b-Instruct-hf"
+model = "meta-llama/CodeLlama-34b-Instruct-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model)
 llm = AutoModelForCausalLM.from_pretrained(
